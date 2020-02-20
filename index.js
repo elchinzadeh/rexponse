47ba451e328e60e4668c5e3a972df53397f0c0d4
Elchin Zakizadeh <elchinzadeh@hotmail.com>
2020-02-20 12:53:58 +0400

    v0.2.0 | Some most used status codes added

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
-function rexponse(code, message) {
-    return {
-        status: code,
-        statusText: message,
+function Rexponse(response, status, payload = null) {
+    let statusText = "";
+
+    switch (status) {
+        case 200:
+            statusText = "OK";
+            break;
+
+        case 201:
+            statusText = "Crated";
+            break;
+
+        case 204:
+            statusText = "No Content";
+            break;
+
+        case 400:
+            statusText = "Bad Request";
+            break;
+
+        case 401:
+            statusText = "Unauthorized";
+            break;
+
+        case 403:
+            statusText = "Forbidden";
+            break;
+
+        case 404:
+            statusText = "Not Found";
+            break;
+
+        case 422:
+            statusText = "Unprocessable Entity";
+            break;
+
+        case 500:
+            statusText = "Internal Server Error";
+            break;
+    }
+
+    const responseBody = {
+        success: status >= 200 && status < 300,
+        status,
+        statusText,
+        payload,
     };
+
+    response.status(status).json(responseBody);
 }
 
-module.exports.rexponse = rexponse;
+module.exports = Rexponse;
